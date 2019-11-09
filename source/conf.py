0bcfceec5907ff381b906ba3ed261d181d173016
arS3Nf2z7DSc01 <nfGuS8kqoz5b@protonmail.com>
2019-11-08 22:19:56 -0800

    docs: initial
    add the giveaway-registration design document

---
+++ source/conf.py
@@ -0,0 +1,15 @@
+project = "bluespan"
+copyright = "2019, Blue Span"
+
+html_theme_options = dict(
+    show_powered_by = False,
+    nosidebar = True,
+)
+
+templates_path = ['_templates']
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/bluespan.css',
+]
